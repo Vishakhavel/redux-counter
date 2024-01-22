dcddee2c4146fe3a5843dd36d8fd71afe75302f2
vishakhavel <vishakhavel.s@capillarytech.com>
2024-01-22 22:22:51 +0530

    removed extra files

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,18 +1,12 @@
 import classes from './Counter.module.css'
 import { useSelector, useDispatch } from 'react-redux'
 const Counter = () => {
-  // useSelector takes a funciton as an argument.
-  // the state managed by redux and the slice needed is returned
+
   const counter = useSelector((state) => state.counter)
 
   const show = useSelector((state) => state.showCounter)
 
-  // managed redux state is taken in, and the counter from the state is returned
-
-  // COOL: REACT-REDUX WILL AUTOMATICALLY SETUP A SUBSCRIPTION WHEN USING USESELECTOR!!!!
-
   const dispatch = useDispatch()
-  //  NO arguments. useDispatch() just returns a dispatch function that we can use to dispatch an action
 
   const incrementHandler = () => {
     dispatch({ type: 'INC' })
@@ -32,13 +26,16 @@ const Counter = () => {
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      {show && <div className={classes.value}>{counter}</div>}
+      {show && <div className={classes.value}>Counter value : {counter}</div>}
+      {!show && <div className={classes.value}>Counter hidden!</div>}
       <div>
         <button onClick={incrementHandler}>+</button>
         <button onClick={incrementByFiveHandler}>+5</button>
         <button onClick={decerementHandler}>-</button>
       </div>
-      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+      <button onClick={toggleCounterHandler}>
+        {show ? 'Hide' : 'Show'}
+      </button>
     </main>
   )
 }
